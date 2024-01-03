d99bf47ea182db6579523a848276313b0e1090ec
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-01-03 14:08:14 -0700

    Create ship class

---
+++ lib/ship.rb
@@ -0,0 +1,18 @@
+class Ship
+  attr_reader :name, :health, :length
+
+  def initialize(name, health)
+    @name = name
+    @health = health
+    @length = lenght
+  end
+
+  def hit
+    @health -= 1
+  end
+
+  def sunk?
+    @health <= 0
+  end
+
+end
