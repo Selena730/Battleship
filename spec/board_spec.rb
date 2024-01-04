58efbe1299b01ddce57dc073ed27ce02adc7c587
lance butler <lancej2004@gmail.com>
2024-01-03 22:04:35 -0700

    makes new describe and it blocks for range method.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -30,16 +30,21 @@ Rspec.describe Board do
     end
 
   describe '#valid_coordinate?' do
+   it "has valid coordinates" do
+
    expect(@board.valid_coordinate?("A1")).to be(true)
    expect(@board.valid_coordinate?("D4")).to be(true)
    expect(@board.valid_coordinate?("A5")).to be(false)
    expect(@board.valid_coordinate?("E1")).to be(false)
    expect(@board.valid_coordinate?("A22")).to be(false)
+   end
   end 
 
 
   
   describe '#valid_placement?' do
+   it "has valid ship placements" do
+
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)    
   
@@ -51,7 +56,31 @@ Rspec.describe Board do
     expect(@board.valid_placement?(submarine, ["C1", "B1"])).to be(false)
     expect(@board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be(false)
     expect(@board.valid_placement?(submarine, ["C2", "D3"])).to be(false)
+    expect(@board.valid_placement?(submarine, ["A1", "A2"])).to be(true)
+    expect(@board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be(true)
+   end
   end
+
+  describe '#range' do
+    it "crates a range method" do
+        range = 3..8
+        
+        expect(range.class).to be_instance_of(Range)
+    end
+
+    it "changes range from int to string" do
+        range = 3..8
+
+        expect(array = range.to_a).to eq([3, 4, 5, 6, 7, 8])
+        expect(array.length).to eq(6)
+        expect(array[3]).to eq(6)
+
+        expect(range = "A".."D").to eq("A".."D")
+        expect(range.to_a).to eq(["A", "B", "C", "D"])
+    end
+  end
+
+
 end
 
 
