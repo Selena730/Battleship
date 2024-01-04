010d8d621b333e0d0f219d32a94886ffe582c39f
lance butler <lancej2004@gmail.com>
2024-01-04 15:18:58 -0700

    created test for place_ship methods and updated some it blocks into valid_placement? method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -43,57 +43,93 @@ Rspec.describe Board do
 
   
   describe '#valid_placement?' do
-   it "has valid ship placements" do
-
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)    
-  
-    expect(@board.valid_placement?(cruiser, ["A1", "A2"])).to be(true)
-    expect(@board.valid_placement?(submarine, ["A2", "A3", "A4"])).to be(true)
-    expect(@board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be(false)
-    expect(@board.valid_placement?(submarine, ["A1", "C1"])).to be(false)
-    expect(@board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be(false)
-    expect(@board.valid_placement?(submarine, ["C1", "B1"])).to be(false)
-    expect(@board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be(false)
-    expect(@board.valid_placement?(submarine, ["C2", "D3"])).to be(false)
-    expect(@board.valid_placement?(submarine, ["A1", "A2"])).to be(true)
-    expect(@board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be(true)
+   before(:each) do
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)  
    end
-  end
 
-  describe '#range' do
-    before(:each) do
-        @range = 3..8
+    it "can identify invalid amount of cells holding length of ship corretly" do
 
-        expect(@range).to be_instance_of(Range)
+        expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be(false)
+        expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be(false)
+    end
+    
+    it "can identify invalid order of consecutive cells" do
+        expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be(false)
+        expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be(false)
+        expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be(false)
+        expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be(false)
+    end
+    
+    it "can register invalid placement (diagonal)" do
+        expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be(false)
+        expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be(false)
     end
 
-    it "crates a range method" do
-        range = 3..8
+    it "can identify valid ship placements" do
+        expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be(true)
+        expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be(true)
+    end
+  end
+
+#   describe '#range' do
+#     before(:each) do
+#         @range = 3..8
+
+#         expect(@range).to be_instance_of(Range)
+#     end
+
+#     it "crates a range method" do
+#         range = 3..8
         
-        expect(range.class).to be_instance_of(Range)
+#         expect(range.class).to be_instance_of(Range)
+#     end
+
+#     it "changes range from int to string" do
+#         range = "A".."D"
+
+#         expect(array = range.to_a).to eq([3, 4, 5, 6, 7, 8])
+#         expect(array.length).to eq(6)
+#         expect(array[3]).to eq(6)
+
+#         expect(range = "A".."D").to eq("A".."D")
+#         expect(range.to_a).to eq(["A", "B", "C", "D"])
+#     end
+
+#     it "can find original value of range" do
+#         range = "A".."D"
+
+#         expect("A".ord).to eq(65)
+#         expect("D".ord).to eq(68)
+#     end
+#   end
+
+  describe '#place_ship' do
+    before(:each) do
+        @cruiser = Ship.new("Cruiser", 3)    
+
     end
 
-    it "changes range from int to string" do
-        range = "A".."D"
-
-        expect(array = range.to_a).to eq([3, 4, 5, 6, 7, 8])
-        expect(array.length).to eq(6)
-        expect(array[3]).to eq(6)
-
-        expect(range = "A".."D").to eq("A".."D")
-        expect(range.to_a).to eq(["A", "B", "C", "D"])
+    it "allows user to place ship" do
+        
+        @board.place(@cruiser, ["A1", "A2", "A3"])    
+        cell_1 = @board.cells["A1"]    
+        cell_2 = @board.cells["A2"]
+        cell_3 = @board.cells["A3"]    
+        
+        expect(cell_1.ship).to eq("A1")
+        expect(cell_2.ship).to eq("A2")
+        expect(cell_3.ship).to eq("A3")
+        expect(cell_3.ship == cell_2.ship).to be(true)
     end
 
-    it "can find original value of range" do
-        range = "A".."D"
-
-        expect("A".ord).to eq(65)
-        expect("D".ord).to eq(68)
+    it "doesn't allow overlapping ships" do
+        submarine = Ship.new("Submarine", 2)    
+        
+        @board.place(@cruiser, ["A1", "A2", "A3"])
+        expect(@board.valid_placement?(submarine, ["A1", "B1"])).to be(false)
     end
   end
-
-
 end
 
 
