af8c8530769eceae9a35b8bb4a813a371c3ba8bf
lance butler <lancej2004@gmail.com>
2024-01-06 14:44:41 -0700

    Created new tests for initialize.

---
+++ spec/game_spec.rb
@@ -0,0 +1,34 @@
+require './lib/game'
+require './lib/cell'
+require './lib/ship'
+require './lib/board'
+
+RSpec.describe Game do
+  before(:each) do
+    @game = Game.new
+  end
+  
+  describe '#initialize' do
+    it "exists and establish boards" do
+      player_board = Board.new
+      computer_board = Board.new
+    
+    
+      expect(player_board).to be_instance_of(Board)
+      expect(computer_board).to be_instance_of(Board)
+    end
+  
+    it "exists and establish ships" do
+      player_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
+      computer_ships = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
+    
+      
+      expect(player_ships).to all(be_a(Ship))
+      expect(computer_ships).to all(be_a(Ship))
+    end
+  end
+end
+  
+
+
+
